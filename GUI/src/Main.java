d3186c792efdfef9e5078a4f84f6fc18f7076b23
slavamfr <muhammadfaizramdhan@mail.ugm.ac.id>
2024-06-05 10:00:29 +0700

    mencoba commmend

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import  javax.swing.*;
 
+//mencoba main
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
